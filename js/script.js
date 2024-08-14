46b52f72a2729ba2b655a5a38606e16cebe69e29
Ilya <ilya02989@gmail.com>
2024-08-14 11:17:34 +0300

    timer done

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //показываем контент табов, i = 0 значение по умолчанию
     function showTabContent(i = 0) {
         tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');        
+        tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
 
@@ -44,5 +44,62 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // Timer
+
+    const deadLine = '2024-08-20';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(deadLine) - Date.parse(new Date()), // превращаем строку в милисекунды для математических операции
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        //Возвращаем вычисления
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    //если одно значное число то подставляет нолик
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock(); // вызываем чтобы не было мигания верстки в начале прогрузки страницы
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    
+    setClock('.timer', deadLine);
 });
 
