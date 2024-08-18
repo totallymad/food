23ce049f77fdd14e823a18515a4e253ffeb23d64
Ilya <example@gmail.com>
2024-08-18 21:45:11 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -48,12 +48,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const deadLine = '2024-08-20';
 
-    function getTimeRemaining(endtime) {
+    function getTimeRemaining(endTime) {
         const t = Date.parse(deadLine) - Date.parse(new Date()), // превращаем строку в милисекунды для математических операции
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
 
         //Возвращаем вычисления
         return {
@@ -76,18 +76,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-    function setClock(selector, endtime) {
+    function setClock(selector, endTime) {
         const timer = document.querySelector(selector),
-              days = timer.querySelector('#days'),
-              hours = timer.querySelector('#hours'),
-              minutes = timer.querySelector('#minutes'),
-              seconds = timer.querySelector('#seconds'),
-              timeInterval = setInterval(updateClock, 1000);
-        
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
         updateClock(); // вызываем чтобы не было мигания верстки в начале прогрузки страницы
 
         function updateClock() {
-            const t = getTimeRemaining(endtime);
+            const t = getTimeRemaining(endTime);
 
             days.innerHTML = getZero(t.days);
             hours.innerHTML = getZero(t.hours);
@@ -99,7 +99,56 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
-    
+
     setClock('.timer', deadLine);
+
+
+    // modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 3000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
 
